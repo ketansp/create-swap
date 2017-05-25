d3ea9527ae3d6be4c1017ba34f71eebefe647bbc
Ketan Patil <ketan@retailwhizz.com>
2017-05-25 20:32:09 +0530

    Adding same script for centos

---
+++ create-swap-on-centos.sh
@@ -0,0 +1,7 @@
+sudo swapon -s
+dd if=/dev/zero of=swapfile count=4096 bs=1MiB
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon -s
+sudo bash -c 'echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab'
